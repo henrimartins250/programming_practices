837c34da6c8a0db79a2831bfb98e1b098d5831a3
henrique <henrinkcstd@gmail.com>
2026-02-25 15:47:07 +0000

    fix: changed grade type from 0-100 to 0-10

--- grades/src/main.js
+++ grades/src/main.js
@@ -6,9 +6,11 @@ for (let i = 0; i < 10; i++) {
   alunos.push({
   nome: faker.person.fullName(),
   notas: [
-  faker.number.int({ min: 1, max: 100 }),
-  faker.number.int({ min: 1, max: 100 })
+  faker.number.int({ min: 1, max: 10 }),
+  faker.number.int({ min: 1, max: 10 })
   ]
+
+  
   });
     
 }
